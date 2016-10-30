8d32a2ef0c6f908e448a4dbeb0b20f83b2990a28
cloudera <cloudera@quickstart.cloudera>
2016-10-30 16:34:20 -0700

    Almost complete the class

--- hw1_src/src/main/java/Stocks/MR2Screener1.java
+++ hw1_src/src/main/java/Stocks/MR2Screener1.java
@@ -4,9 +4,8 @@ import java.io.IOException;
 import java.util.Calendar;
 
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Counter;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -17,7 +16,8 @@ import org.apache.log4j.Logger;
 public class MR2Screener1 {
 	static Logger logger = Logger.getLogger(MR2Screener1.class);
 
-	public static class SumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+	public static class SectorReducer extends Reducer<Text, Text, Text, Text> {
+		private final static String SECTOR_COUNT_LABEL = "SectorCount";
 
 		/**
 		 * The reduce method runs once for each key received from the shuffle
@@ -31,15 +31,25 @@ public class MR2Screener1 {
 		 *            info about the job's config, writers for output
 		 */
 		@Override
-		public void reduce(Text key, Iterable<IntWritable> values, Context context)
-				throws IOException, InterruptedException {
+		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
 
-			int wordCount = 0;
+			logger.info("In reduce()");
+			StringBuffer symbols = new StringBuffer();
+			double sectorTotalCap = 0.0;
 
-			for (IntWritable value : values)
-				wordCount += value.get();
+			for (Text value : values) {
+				logger.info("key=[" + key + "] value=" + value);
+				String[] symbolDetails = value.toString().split("===");
+				String capStr = symbolDetails[1].substring(1, symbolDetails[1].length() - 1);
+				sectorTotalCap += new Double(capStr);
 
-			context.write(key, new IntWritable(wordCount));
+				symbols.append(", " + symbolDetails[0]);
+			}
+
+			Counter totalCompanies = context.getCounter(SECTOR_COUNT_LABEL, key.toString());
+
+			context.write(key, new Text(": Total Companies: " + totalCompanies.getValue() + "; Total Market Cap: $"
+					+ sectorTotalCap + "B SYMBOLS: " + symbols.toString()));
 		}
 
 	}
@@ -58,7 +68,21 @@ public class MR2Screener1 {
 	 * @param IntWritable
 	 *            type of output value (same type for Reducer input value)
 	 */
-	public static class SectorMapperWithCounter extends Mapper<LongWritable, Text, Text, IntWritable> {
+	public static class SectorMapperWithCounter extends Mapper<Object, Text, Text, Text> {
+		private final static String recordRegex = ",(?=([^\"]*\"[^\"]*\")*[^\"]*$)";
+		private final static String SECTOR_COUNT_LABEL = "SectorCount";
+		private final static int sectorIndex = 5;
+		private final static int capIndex = 3;
+		public final static String NO_INFO = "n/a";
+
+		@Override
+		protected void cleanup(/* Mapper<Object, Text, Text, Text>. */Context context)
+				throws IOException, InterruptedException {
+			// context.write(new Text("Nothing to view here - all the info isin
+			// the counters."), Text.get());
+			logger.info("in cleanup()");
+			super.cleanup(context);
+		}
 
 		/*-
 		 * This function splits each input line into an array of words.
@@ -66,16 +90,32 @@ public class MR2Screener1 {
 		 *
 		 */
 		@Override
-		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 
-			String line = value.toString();
+			String[] tokens = value.toString().split(recordRegex, -1);
+			String symbol = tokens[0];
+			String sectorStr = tokens[5];
+			String capStr = tokens[capIndex].replace("\"", "");
+			// logger.info("key=[" + key + ", " + value.toString());
+			// logger.info("Token len=" + tokens.length + " sector=[" +
+			// sectorStr + "] capStr=[" + capStr);
 
-			for (String word : line.split("\\W+")) {
-				if (word.length() > 0) {
-					context.write(new Text(word), new IntWritable(1));
-				}
+			if (tokens.length == 9 && !tokens[1].equalsIgnoreCase("Sector") && !sectorStr.equalsIgnoreCase(NO_INFO)
+					&& capStr.endsWith("B")) {
+				context.write(new Text(sectorStr), new Text(symbol + "===" + capStr));
+				// logger.info("Found a B-company");
+				context.getCounter(SECTOR_COUNT_LABEL, sectorStr).increment(1);
+				context.getCounter(SECTOR_COUNT_LABEL, "Totalcompanies processed successfully").increment(1);
 			}
+
 		}
+		/***
+		 * @Override protected void setup(Mapper<Object, Text, Text,
+		 *           Text>.Context context) throws IOException,
+		 *           InterruptedException { // TODO Auto-generated method stub
+		 *           //super.setup(context); Configuration conf =
+		 *           context.getConfiguration(); sector = }
+		 ***/
 	}
 
 	/**
@@ -110,19 +150,19 @@ public class MR2Screener1 {
 		 * Specify the mapper and reducer classes.
 		 */
 		job.setMapperClass(SectorMapperWithCounter.class);
-		job.setReducerClass(SumReducer.class);
+		job.setReducerClass(SectorReducer.class);
 
 		/*
 		 * Specify the number of reduce tasks
 		 *
 		 */
-		job.setNumReduceTasks(17);
+		job.setNumReduceTasks(1);
 
 		/*
 		 * Specify the job's output key and value classes.
 		 */
 		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(IntWritable.class);
+		job.setOutputValueClass(Text.class);
 
 		/*
 		 * Start the MapReduce job and wait for it to finish. If it finishes
